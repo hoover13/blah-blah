a5a2233958db5c76bc6f61c6828a6ab431b9f7e7
hoover13 <hoovermtk25@gmail.com>
2024-04-02 21:55:55 -0400

    Initial Commit

---
+++ Blah Blah GitHub/Blah_Blah_GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blah_Blah_GitHubApp.swift
+//  Blah Blah GitHub
+//
+//  Created by Min Thu Khine on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Blah_Blah_GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
